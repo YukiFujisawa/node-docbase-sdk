5394a70d97351482cb29c017ab626420b4df394d
Yuki Fujisawa <g031y148yuki@gmail.com>
2018-01-31 18:12:53 +0900

    :gem: Change request lib to 'snekfetch' from 'phin'.

--- lib/DocBaseResponse.d.ts
+++ lib/DocBaseResponse.d.ts
@@ -2,12 +2,14 @@ import { HttpStatus } from './enums/HttpStatus';
 export declare namespace DocBaseResponseFields {
     type status = HttpStatus;
     type statusCode = number;
+    type statusText = string;
     type options = any;
     type body = any;
 }
 export interface DocBaseResponse {
     status: DocBaseResponseFields.status;
     statusCode: DocBaseResponseFields.statusCode;
+    statusText: DocBaseResponseFields.statusText;
     options: DocBaseResponseFields.options;
     body: DocBaseResponseFields.body;
 }
